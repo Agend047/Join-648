61b822b121408729dedd4ed74d3fa7cb19859243
Jan Killburger <jkillburger@gmail.com>
2023-08-05 18:23:41 +0200

    added desktop version of splash login screen

--- js/login.js
+++ js/login.js
@@ -1,3 +1,10 @@
+function init() {
+    checkWidth();
+    if (loaded === 'desktop') {
+        document.getElementById('login-logo').src = './assets/img/join_logo_black.png';
+    }
+    animateSplashScreen();
+}
 /**Splash screen animation: This function coordinates the splash screen animation by calling three sub-functions with increasing timeouts. */
 function animateSplashScreen() {
     const SPLASH_DURATION = 1000;
@@ -12,7 +19,7 @@ function startSplashAnimation() {
 }
 /**Splash screen animation: This function changes the logo from the white to the black one during the splash screen animation. */
 function changeLogo() {
-    document.getElementById('login-logo').src = './assets/img/join_logo_black.png'
+    document.getElementById('login-logo').src = './assets/img/join_logo_black.png';
 }
 /**Splash screen animation: This function adds and removes classes to achieve the end result of the animation. */
 function removeSplash() {
