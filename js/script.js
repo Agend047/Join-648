c663907488f19b709c2721e6adecd86f5051e9d5
Agend047 <schneiderniklas@gmx.de>
2023-08-05 16:46:42 +0200

    Check width, template functions
    Functions to check width of  window to load in or unload desktop/mobile templates.
    (And additional use!)
    Important Variables:
    docWidth: Width of html Document
    loaded: is "mobile" or "desktop"
    
    MFG

--- js/script.js
+++ js/script.js
@@ -1,20 +1,60 @@
-function checkWidth() { }
+let docWidth;
+let loaded;
 
-
-async function getHeader() {
-    await includeHTML();
+/**
+ * Checks width of shown screen, loads templates as needed and saves wich version has been loaded in global Variable.
+ */
+function checkWidth() {
+    let htmlDocument = document.getElementsByTagName('html');
+    docWidth = htmlDocument[0].offsetWidth;
+    if (docWidth < 600) {
+        getMatchingTemplate('mobile-template', 'desktop-template') //HERE
+        loaded = 'mobile'
+    } else if (docWidth > 600) {
+        getMatchingTemplate('desktop-template', 'mobile-template') //HERE
+        loaded = 'desktop'
+    }
 }
 
-async function includeHTML() {
-    let includeElements = document.querySelectorAll('[w3-include-html]');
-    for (let i = 0; i < includeElements.length; i++) {
-        const element = includeElements[i];
-        file = element.getAttribute("w3-include-html"); // "assets/templates/desktop_template.html"
-        let resp = await fetch(file);
-        if (resp.ok) {
-            element.innerHTML = await resp.text();
-        } else {
-            element.innerHTML = 'Page not found';
-        }
+/**
+ * When Window gets resized, checks if the other template may have to be loaded.
+ */
+window.onresize = function () {
+    let htmlDocument = document.getElementsByTagName('html');
+    docWidth = htmlDocument[0].offsetWidth;
+    if (docWidth < 600 && loaded == 'desktop') {
+        getMatchingTemplate('mobile-template', 'desktop-template') //HERE
+        // getMobileTemplates()
+        loaded = 'mobile';
+    } else if (docWidth > 600 && loaded == 'mobile') {
+        getMatchingTemplate('desktop-template', 'mobile-template') //HERE
+        // getDesktopTemplates()
+        loaded = 'desktop';
     }
+}
+
+/**
+ * Loads in templates for needed Version, (Mobile or Desktop) unloads the other templates, if needed.
+ */
+async function getMatchingTemplate(toLoadID, toUnloadID) {
+    let includeElement = document.getElementById(toLoadID);
+    const element = includeElement;
+    file = element.getAttribute("include-templates"); // "assets/templates/desktop_template.html" or mobile_template.html
+    let resp = await fetch(file);
+    if (resp.ok) {
+        element.innerHTML = await resp.text();
+    } else {
+        element.innerHTML = 'Page not found';
+    }
+    unloadTemplate(toUnloadID)
+}
+
+
+/**
+ * Undloads the not needed Template.
+ * @param {*} toUnloadID ID of to unload Template
+ */
+function unloadTemplate(toUnloadID) {
+    let toUnload = document.getElementById(toUnloadID);
+    toUnload.innerHTML = '';
 }
