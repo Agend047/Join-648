62893b2ec36819c1f3cc87733732cf5280e3c10a
SusanneRobledo <susanne.robledo@web.de>
2023-08-10 16:44:25 +0200

    Corrected HelloPageMobile / Fade out Transition

--- js/summary.js
+++ js/summary.js
@@ -1,10 +1,30 @@
-function removeHelloPageMobile() {
-    let helloPage = document.getElementById("helloPageMobile");
-    helloPage.classList.add("fadeOut");
+function loadHelloPageMobile() {
+    let htmlDocument = document.getElementsByTagName('html');
+    docWidth = htmlDocument[0].offsetWidth;
+    if (docWidth < 820) {
 
-    helloPage.addEventListener("transitionend", function () {
-    helloPage.style.display = "none";
-    });
+        let helloPage = document.createElement("div");
+        helloPage.innerHTML = `
+            <div id="helloPageMobile" class="">
+            <span>Good morning,</span>
+            <h1 class="blue-text">Sofia Müller</h1>
+            </div>
+            `;
+        
+        // add the newly created element and its content into the DOM
+        let main = document.querySelector('main');
+
+        // Insert the helloPage before the mobileTemplate
+        main.parentNode.insertBefore(helloPage, main);
+        
+        setTimeout(function() {
+            helloPage.classList.add("fadeOut");
+        }, 1500);
+
+        helloPage.addEventListener("transitionend", function () {
+        helloPage.style.display = "none";
+        })
+    } 
 }
 
 function changeIcon(Id, url) {
