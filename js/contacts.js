1200158a5bb36042024bb531f89a18348eab065b
Agend047 <schneiderniklas@gmx.de>
2023-08-11 16:12:02 +0200

    Contacts update for today

--- js/contacts.js
+++ js/contacts.js
@@ -1,3 +1,32 @@
+let exampleContacts = [
+    {
+        startingLetter: 'A',
+        name: 'Anja Schulz',
+        e_mail: 'schulz@hotmail.com',
+        phone: 9102423513,
+        initials: 'AS',
+        color: 'orange',
+    },
+    {
+        startingLetter: 'B',
+        name: 'Benjamin Schulz',
+        e_mail: 'bschulz@hotmail.com',
+        phone: 0o742334542,
+        initials: 'BS',
+        color: 'blue',
+    }
+
+]
+
+
 function startAddContact() {
     alert('Patience!')
+}
+
+function editContact() {
+    alert('Edit')
+}
+
+function deleteContact() {
+    alert('Delete!')
 }
