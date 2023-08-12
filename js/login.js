74daeeb033a06b5504f422391be0f48899871bba
Jan Killburger <jkillburger@gmail.com>
2023-08-12 15:04:46 +0200

    completed first versions of initial screens and linked them across each other

--- js/login.js
+++ js/login.js
@@ -1,20 +1,38 @@
-const splashLoginBgEl = document.getElementById('splash-login-bg');
-const loginLogoEl = document.getElementById('login-logo');
-const loginLogoStaticEl = document.getElementById('login-logo-static');
 const headerEl = document.getElementById('header');
 let currentFormKey = 'login';
 
 function init() {
-    checkWidth(true);
-    if (loaded === 'desktop') {
-        loginLogoEl.src = './assets/img/join_logo_black.png';
+    const appStarted = getSessionData();
+    if (!appStarted) {
+        getAnimationHtml();
+        const loginLogoEl = document.getElementById('login-logo');
+        checkWidth(true);
+        if (loaded === 'desktop') {
+            loginLogoEl.src = './assets/img/join_logo_black.png';
+        }
+        animateSplashScreen();
     }
-    animateSplashScreen();
     // renderMainContent(currentFormKey, true);
     initLoginForm();
     initSignUpBtns();
     initCheckboxes();
     // initBackNavIcon();
+}
+
+function getAnimationHtml() {
+    document.body.innerHTML += `<div id="splash-login-bg"></div>
+    <img
+      src="./assets/img/join_logo_white.png"
+      alt="Join logo"
+      class="img-large splash-active"
+      id="login-logo"
+    />`;
+}
+
+function getSessionData() {
+    const appStarted = JSON.parse(sessionStorage.getItem('appStarted'));
+    if (!appStarted) { sessionStorage.setItem('appStarted', 'true'); }
+    return appStarted;
 }
 
 function initCheckboxes() {
@@ -42,15 +60,23 @@ function animateSplashScreen() {
 }
 /**Splash screen animation: This functions starts the splash screen animation by adding the 'splash-moving' class to the logo and background container. */
 function startSplashAnimation() {
+    const loginLogoStaticEl = document.getElementById('login-logo-static');
+    const splashLoginBgEl = document.getElementById('splash-login-bg');
+    const loginLogoEl = document.getElementById('login-logo');
     loginLogoEl.classList.add('splash-moving');
     splashLoginBgEl.classList.add('splash-moving');
+    loginLogoStaticEl.classList.add('splash-active');
 }
 /**Splash screen animation: This function changes the logo from the white to the black one during the splash screen animation. */
 function changeLogo() {
+    const loginLogoEl = document.getElementById('login-logo');
     loginLogoEl.src = './assets/img/join_logo_black.png';
 }
 /**Splash screen animation: This function adds and removes classes to achieve the end result of the animation. */
 function removeSplash() {
+    const splashLoginBgEl = document.getElementById('splash-login-bg');
+    const loginLogoEl = document.getElementById('login-logo');
+    const loginLogoStaticEl = document.getElementById('login-logo-static');
     splashLoginBgEl.classList.add('d-none');
     splashLoginBgEl.classList.remove('splash-moving');
     loginLogoEl.classList.remove('splash-moving');
