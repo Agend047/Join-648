8087e7677ead778a62e0ba73ec36eddf9501ea11
SusanneRobledo <susanne.robledo@web.de>
2023-08-15 18:30:13 +0200

    Board Placeholder solved with HTML

--- js/board.js
+++
@@ -1,9 +0,0 @@
-function noTasksPlaceholder(colID, colTitle) {
-  let content = document.getElementById(colID);
-
-  if (content.innerHTML === "") {
-    content.innerHTML = `
-    <div class="placeholder">No tasks ${colTitle}</div>
-`;
-  }
-}
