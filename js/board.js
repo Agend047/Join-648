8ca46bc4139b48cb38f41094ddebc0ff222d59fd
SusanneRobledo <susanne.robledo@web.de>
2023-08-16 18:54:35 +0200

    Board - Started PopUp Task HTML / CSS

---
+++ js/board.js
@@ -0,0 +1,10 @@
+function openCard() {
+  let largeCard = document.getElementById("popUpContainer");
+  document.body.style.overflow = "hidden";
+  largeCard.innerHTML = /*html*/ `
+    
+
+`;
+}
+
+function closeCard(i) {}
