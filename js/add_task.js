3a20b27623704afeed64e5ac416ac18c39f873b6
Jan Killburger <jkillburger@gmail.com>
2023-08-17 17:25:44 +0200

    added functions to subtask input

--- js/add_task.js
+++ js/add_task.js
@@ -1,7 +1,11 @@
 const subtaskEl = document.getElementById('subtasks-container');
+let subtasks = [];
 function initSubtaskInput() {
     subtaskEl.firstElementChild.addEventListener('focus', toggleSubtaskIcons);
     subtaskEl.firstElementChild.addEventListener('blur', toggleSubtaskIcons);
+    document.getElementById('add-subtask').addEventListener('click', () => setFocusToElement(subtaskEl.firstElementChild));
+    document.getElementById('cancel-subtask').addEventListener('click', cancelSubtask);
+    document.getElementById('save-subtask').addEventListener('click', saveSubtask);
 }
 
 function toggleSubtaskIcons() {
@@ -12,6 +16,24 @@ function toggleSubtaskIcons() {
             element.classList.toggle('d-none');
         }
     }
+}
+
+function setFocusToElement(element) {
+    element.focus();
+}
+
+function cancelSubtask() {
+    document.getElementById('subtasks-input').value = '';
+    toggleSubtaskIcons();
+}
+
+function saveSubtask() {
+    const input = document.getElementById('subtasks-input');
+    subtasks.push(input.value);
+    input.value = '';
+    input.focus();
+    toggleSubtaskIcons();
+    console.log(subtasks);
 }
 
 // window.onload = initSubtaskInput;
