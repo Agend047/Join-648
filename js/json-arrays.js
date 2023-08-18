c8b564fe430b03bd5a9bb9128aeb7d96489ce1a9
Jan Killburger <jkillburger@gmail.com>
2023-08-18 21:44:35 +0200

    added json structures

--- js/json-arrays.js
+++ js/json-arrays.js
@@ -11,4 +11,40 @@ let contact = {
     phone: 9102423513,
     initials: 'AS',
     color: '#FF7A00',
-}a,
+    id: 1
+};
+
+let task = {
+    title: "title",
+    description: "längerer Text",
+    dueDate: "dd/mm/yyyy",
+    prio: "urgent",
+    category: "user story",
+    assignedTo: [
+        {
+            startingLetter: 'A',
+            name: 'Anja Schulz',
+            e_mail: 'schulz@hotmail.com',
+            phone: 9102423513,
+            initials: 'AS',
+            color: '#FF7A00',
+        }
+    ],
+    subtasks: [
+        "subtask1",
+        "subtask2"
+    ]
+}
+
+let task2 = {
+    title: "title",
+    description: "längerer Text",
+    dueDate: "dd/mm/yyyy",
+    prio: "urgent",
+    category: "user story",
+    assignedTo: [3, 6, 10],
+    subtasks: [
+        "subtask1",
+        "subtask2"
+    ]
+}
