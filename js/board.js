1ad276e979a0ea3776eacfdf2d81bfc7935a25d3
Jan Killburger <jkillburger@gmail.com>
2023-08-24 19:15:15 +0200

    added draggable attribute

--- js/board.js
+++ js/board.js
@@ -59,7 +59,7 @@ function assignLabelColor(category) {
 
 function generateSmallCardHTML(totalSubtasks, task, i, labelColor) {
   return /*html*/ `
-              <div id="cardSmall-${i}" class="cardSmall" onclick="openCard(${i})">
+              <div id="cardSmall-${i}" draggable="true" class="cardSmall" onclick="openCard(${i})">
                 <div class="category">
                   <div class="categoryLabel" style="background: ${labelColor};" id="categoryLabel">${task.category}</div>
                 </div>
